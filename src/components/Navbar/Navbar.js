9b87ad7e889d0ad3d15c902007809db7b0ad8f57
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-05 21:32:48 +0545

    Basic Navigation bar added with styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
+import AppLogo from '../../images/SpaceAppLogo.png';
 
 const Navbar = () => {
+  const location = useLocation();
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <header>
+        <nav className="navbar">
+          <div className="title-div">
+            <img src={AppLogo} className="logo" alt="logo" />
+            <h1>Space Traveller&lsquo;s Hub</h1>
+          </div>
+          <ul className="links-ul">
+            <li>
+              <Link
+                to="/"
+                className={location.pathname === '/' ? 'active' : ''}
+              >
+                {' '}
+                Rockets
+                {' '}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/missions"
+                className={location.pathname === '/missions' ? 'active' : ''}
+              >
+                {' '}
+                Missions
+                {' '}
+              </Link>
+            </li>
+            <li>|</li>
 
-export default Navbar
+            <li>
+              <Link
+                to="/profile"
+                className={location.pathname === '/profile' ? 'active' : ''}
+              >
+                {' '}
+                Profile
+                {' '}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
