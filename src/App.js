c1754549b97f16de8d1c0cf21e00e9b2e9544dac
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-05 21:20:45 +0545

    Basic routing setup & file setup for naviagation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Rockets from './pages/Rockets/Rockets';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="">
+          <Routes>
+            <Route path="/" element={<Rockets/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
