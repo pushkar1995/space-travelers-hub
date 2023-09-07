7e8d36695b9c01a27a757a5a9357b3f1a572e1a4
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-09-08 00:14:29 +0500

    basic setup complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketSliceReducer from './rocketSlice';
+import missionSliceReducer from './missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketSliceReducer,
+    missions: missionSliceReducer,
   },
 });
 
