87508568969f347912ce33480a6b0bcca8c43915
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-07 14:33:48 +0545

    Rocket Badge added as per the requirement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'; // Import Provider from react-redux
 import store from './redux/store'; // Import your Redux store
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
