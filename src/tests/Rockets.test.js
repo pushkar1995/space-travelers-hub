20cff4b3900238c33cb41a92c0d0096df05a9278
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-09 17:47:28 +0545

    Fix test according to the review of the reviewer

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -11,22 +11,24 @@ jest.mock('axios', () => ({
   get: jest.fn(),
 }));
 
-test('Rockets component renders correctly', () => {
+describe('Rockets Component', () => {
+  test('Rockets component renders correctly', () => {
   // Mock the useSelector behavior
-  useSelector.mockReturnValue({
-    rockets: [
-      {
-        id: 1,
-        name: 'Falcon 9',
-        description: 'A powerful rocket developed by SpaceX.',
-        flickr_images: ['image1.jpg', 'image2.jpg'],
-        reserved: false,
-      },
+    useSelector.mockReturnValue({
+      rockets: [
+        {
+          id: 1,
+          name: 'Falcon 9',
+          description: 'A powerful rocket developed by SpaceX.',
+          flickr_images: ['image1.jpg', 'image2.jpg'],
+          reserved: false,
+        },
       // Add more mock rockets as needed
-    ],
+      ],
+    });
+
+    const { container } = render(<Rockets />);
+
+    expect(container).toMatchSnapshot();
   });
-
-  const { container } = render(<Rockets />);
-
-  expect(container).toMatchSnapshot();
 });
